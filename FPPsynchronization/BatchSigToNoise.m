3e81793d58e9003bd8d3c66743c5e8aac4c7d402
Kristian Weegink <k.weegink@gmail.com>
2013-05-28 15:27:40 +1000

    Synchronization range
    Selecting the range of synchronization for paper

--- FPPsynchronization/BatchSigToNoise.m
+++ FPPsynchronization/BatchSigToNoise.m
@@ -1,7 +1,7 @@
 
 bar=waitbar(0,'Repeat simulations');
 
-percenters=[0.005, 0.05, 0.2, 0.5, 0.7, 0.9];
+percenters=[0.001 0.005 0.007 0.01 0.02 0.05 0.07 0.1 0.15 0.2 0.25 0.3];
 Niterations=20;
 plen=length(percenters);
 

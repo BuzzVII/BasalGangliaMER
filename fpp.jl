2c1dffdd09b963403af02f1b3ea733b256b62d02
Kristian <k.weegink@gmail.com>
2017-02-15 22:42:42 +1000

    Setting up a RK solver with a delay

--- fpp.jl
+++ fpp.jl
@@ -1,17 +1,20 @@
 using Plots
 
-function observability(C,A)
-  D = C
-  for n  = 1:(size(A)[1] - 1)
-    D = [D; C*A^n]
+function h(t, rate, dt, j)
+  k = floor(Int,t/dt)
+  i = 1
+  if k > j[1]
+    i = j[1]
+  elseif k >= 1
+    i = k
   end
-  rank(D)
+  return rate[i]
 end
 
 ξ = 1.0
 N = 10000
 T = 1.0
-ve(t) = 14.0 - 4.0*sin(2*30.0*pi*t) + 4*rand()
+ve(t) = 14.0 - 4.0*sin(2*30.0*pi*t)
 delays = true
 
 #simulation paramters
@@ -34,27 +37,23 @@ neuron_superposition = zeros(Int(fs*T))
 rates = zeros(5, Int(fs*T))
 U = [9.293; -18.4203; 3.857; -4.0643; 16.2714; -194.239; 11.39; -135.967; -1.544; -18.446]
 rates[:,1] = [ζ(U[1], θ_p1, k_p1, S_p1) ζ(U[3], θ_p2, k_p2, S_p2) ζ(U[5], θ_d1,  k_d1,  S_d1) ζ(U[7], θ_d2,  k_d2,  S_d2) ζ(U[9], θ_Ϛ, k_Ϛ, S_Ϛ)]'
-dU = zeros(10,1)
+k1 = zeros(10,1)
+k2 = zeros(10,1)
+k3 = zeros(10,1)
+k4 = zeros(10,1)
 refactory = zeros(N,1)
 neurons = zeros(N,1)
-C = zeros(10,10)
-C[9,9] = 1; C[10,10] = 1
-obs = zeros(1, Int(fs*T))
-A = [[0        1         0              0                  0        0       0            0           0           0]
-     [(α*β) (β + α) (α*β)*ν[2,1] (α*β)*ν[2,1]     (α*β)*ν[3,1] (α*β)*ν[3,1] 0            0     (α*β)*ν[5,1] (α*β)*ν[5,1]]
-     [0        0         0              1                  0        0       0            0           0           0]
-     [0        0 (α*β)+(α*β)*ν[2,2] (β + α)+(α*β)*ν[2,2]   0        0  (α*β)*ν[4,2] (α*β)*ν[4,2] (α*β)*ν[5,2] (α*β)*ν[5,2]]
-     [0        0         0              0                  0        1       0            0           0           0]
-     [0        0         0              0                (α*β)   (β + α)    0            0           0           0]
-     [0        0         0              0                  0        0       0            1           0           0]
-     [0        0         0              0                  0        0     (α*β)        (β + α)       0           0]
-     [0        0         0              0                  0        0       0            0           0           1]
-     [0        0    (α*β)*ν[2,5]    (α*β)*ν[2,5]           0        0       0            0         (α*β)     (β + α)]]
+j = [1]
+g = [t -> h(t, rates[ceil(Int, k/2),:], dt, j) for k in 1:10]
 
-for i = 2:Int(fs*T)
-    #calculate BG rates
-    bg_model(i*dt,U,dU)
-    U += dU*dt
+for i = 2:floor(Int,fs*T)
+    j[1] = i
+    #calculate BG rates using RK4
+    bg_model(i*dt, U, k1, g)
+    bg_model(i*dt + 0.5*dt, U + 0.5*k1*dt, k2, g)
+    bg_model(i*dt + 0.5*dt, U + 0.5*k2*dt, k3, g)
+    bg_model(i*dt + dt, U + k3*dt, k4, g)
+    U += (k1 + 2*k2 + 2*k3 + k4)*dt/6
     rates[:,i] = [ζ(U[1], θ_p1, k_p1, S_p1) ζ(U[3], θ_p2, k_p2, S_p2) ζ(U[5], θ_d1,  k_d1,  S_d1) ζ(U[7], θ_d2,  k_d2,  S_d2) ζ(U[9], θ_Ϛ, k_Ϛ, S_Ϛ)]
     #Generate MER
     P  = dt * rates[5,i]
